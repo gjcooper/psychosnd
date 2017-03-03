a6b352c00a4c579e52ab5a0557224d5d3ffe14e7
Gavin Cooper <Gavin.Cooper@newcastle.edu.au>
2017-03-03 16:29:02 +1100

    Add scal for psychopy code
    * console script entry point and argument checks
    * Reuse as much as possible from prespy
    * Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
+import subprocess
+import sys
 
 __version__ = '0.0.1'
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+pandoc_call = ['pandoc', '--from=markdown', '--to=rst', 'README.md']
+
+try:
+    output = subprocess.run(pandoc_call, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if output.returncode:
+        print(output.stderr)
+        sys.exit()
+    output = output.stdout
+except AttributeError:
+    try:
+        output = subprocess.check_output(pandoc_call)
+    except subprocess.CalledProcessError:
+        sys.exit()
 
 # get the dependencies and installs
 with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
@@ -21,7 +33,7 @@ setup(
     name='psychosnd',
     version=__version__,
     description='Psychopy sound analysis scripts',
-    long_description=long_description,
+    long_description=output,
     url='https://github.com/gjcooper/psychosnd',
     download_url='https://github.com/gjcooper/psychosnd/tarball/' + __version__,
     license='GPLv3',
@@ -30,11 +42,14 @@ setup(
       'Intended Audience :: Developers',
       'Programming Language :: Python :: 3',
     ],
-    keywords='',
+    keywords=['Psychopy', 'logfile', 'sound'],
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     author='Gavin Cooper',
     install_requires=install_requires,
     dependency_links=dependency_links,
-    author_email='gjcooper@gmail.com'
+    author_email='gjcooper@gmail.com',
+    entry_points={
+        'console_scripts': ['scla=psychosnd.__main__:scla_script'],
+    }
 )
