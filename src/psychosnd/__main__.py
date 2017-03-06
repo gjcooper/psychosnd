845e7c8abeef7f0e608219809fe1509d66b2a8e6
Gavin Cooper <Gavin.Cooper@newcastle.edu.au>
2017-03-06 15:27:25 +1100

    Add version information to command line script

--- src/psychosnd/__main__.py
+++ src/psychosnd/__main__.py
@@ -1,15 +1,18 @@
 from psychosnd.scla import scla, ExtractError
+from psychosnd.__about__ import __version__, __title__
 import sys
 import argparse
 
+
 def scla_script():
-    parser = argparse.ArgumentParser(description='Analyse sound latencies')
+    parser = argparse.ArgumentParser(description='scla {} ({} variant) - Analyse sound latencies'.format(__version__, __title__))
     parser.add_argument('--runid', '-i', help='An id for the sound plot', default='scla')
     parser.add_argument('soundfile', help='A sound recording of port and sound output')
     parser.add_argument('logfile', help='Stimulus delivery recording of event sequence')
     parser.add_argument('--schannel', '-c', help='The channel sounds were recorded in', default=1)
     parser.add_argument('--maxdur', '-m', help='Max duration for sound/port event', default=0.012)
     parser.add_argument('--thresh', '-t', help='Threshold for sound/port detection', default=0.2)
+    parser.add_argument('--version', action='version', version='scla {} ({} variant)'.format(__version__, __title__))
 
     args = parser.parse_args()
 
